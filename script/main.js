ee703b614d68d029f2ed15b23272faf684c7252b
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-09-23 05:30:14 +0200

    Complete all the card

---
+++ script/main.js
@@ -0,0 +1,37 @@
+// toggle between active button
+
+const donationButton = document.getElementById('donationButton');
+const historyButton = document.getElementById('historyButton');
+
+donationButton.addEventListener('click', () => {
+    // Highlight Donation button
+    donationButton.classList.add('bg-[#B4F461]', 'text-[#111111]');
+    donationButton.classList.remove('bg-gray-300', 'text-black');
+
+    // Reset History button
+    historyButton.classList.remove('bg-[#B4F461]', 'text-white');
+    historyButton.classList.add('bg-gray-300', 'text-black');
+});
+
+historyButton.addEventListener('click', () => {
+    // Highlight History button
+    historyButton.classList.add('bg-[#B4F461]', 'text-white');
+    historyButton.classList.remove('bg-gray-300', 'text-black');
+
+    // Reset Donation button
+    donationButton.classList.remove('bg-[#B4F461]', 'text-white');
+    donationButton.classList.add('bg-gray-300', 'text-black');
+});
+
+
+
+// blur when user scroll
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('backdrop-blur-lg', 'bg-opacity-90', '!bg-[#F9F7F3]');
+    } else {
+        header.classList.remove('backdrop-blur-lg', 'bg-opacity-90');
+    }
+});
